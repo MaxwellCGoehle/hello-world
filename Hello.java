a1d04fd7b76a57831ecbe7a54f00b44611c2a41c
maxwell56 <89530666+maxwell56@users.noreply.github.com>
2021-08-25 10:22:35 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/***********************************************************************
+* Hello.java
+************************************************************************
+* Maxwell Goehle
+* 08/25/2021
+* 255-004
+************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Maxwell Goehle>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Lab 1>");
+		System.out.println("<08/25/2020>");
+		System.out.println("<235 04>");
+		System.out.println();
+
+	}
+}
